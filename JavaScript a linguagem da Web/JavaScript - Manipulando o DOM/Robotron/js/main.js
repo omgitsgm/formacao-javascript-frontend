e909f449b036dd96335507eb9d99cbda1c110eea
Gabriel Mendes <luizgabrielpm@gmail.com>
2022-12-20 13:32:59 -0300

    Mudança de Cor do Robotron
    Arquivos de cores e implementação da funcionalidade de mudança de cor.

--- JavaScript a linguagem da Web/JavaScript - Manipulando o DOM/Robotron/js/main.js
+++ JavaScript a linguagem da Web/JavaScript - Manipulando o DOM/Robotron/js/main.js
@@ -1,5 +1,6 @@
 const controle = document.querySelectorAll("[data-controle]");
 const estatisticas = document.querySelectorAll("[data-estatistica]");
+const robotron = document.querySelector("#robotron");
 
 const pecas = {
     "bracos": {
@@ -32,13 +33,28 @@ const pecas = {
         "poder": 28,
         "energia": 0,
         "velocidade": -2
+    },
+    "cores":{
+        "0": "img/Robotron 2000 - Azul.png",
+        "1": "img/Robotron 2000 - Amarelo.png",
+        "2": "img/Robotron 2000 - Branco.png",
+        "3": "img/Robotron 2000 - Preto.png",
+        "4": "img/Robotron 2000 - Rosa.png",
+        "5": "img/Robotron 2000 - Vermelho.png"
     }
 }
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
-        atualizaEstatisticas(evento.target.dataset.peca);
+        if(elemento.dataset.peca != "cores"){
+            // console.log("!cor");
+            atualizaEstatisticas(evento.target.dataset.peca);
+        } else{
+            // console.log(evento.target.parentNode.childNodes[3].value)
+            atualizaCor(evento.target.dataset.peca, evento.target.parentNode.childNodes[3].value)
+        }
+        
     })
 })
 
@@ -59,4 +75,10 @@ function atualizaEstatisticas(peca){
         console.log(elemento.dataset.estatistica);
         elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
     })
+}
+
+function atualizaCor(peca, cor){
+    // console.log(pecas[peca]);
+    // console.log(pecas[peca][cor]);
+    robotron.src = pecas[peca][cor];
 }
