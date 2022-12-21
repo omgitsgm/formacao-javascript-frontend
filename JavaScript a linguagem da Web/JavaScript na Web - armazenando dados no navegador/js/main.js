6e7e05a615a08ce024281878b0dea7e98c1e28d5
Gabriel Mendes <luizgabrielpm@gmail.com>
2022-12-21 18:18:23 -0300

    Proj. Mochila e Proj. Dark
    Dois novos projetos no repositório:
    - Projeto Mochila de Viagem
    	- Projeto que permite o usuário fazer uma lista de itens que
    	  pretende levar numa viagem. O usuário é capaz de inserir,
    	  atualizar e remover itens.
    - Projeto Dark
    	- Projeto que implementa a funcionalidade 'Dark Mode'
    	  numa página HTML/CSS simples, utilizando JS.

---
+++ JavaScript a linguagem da Web/JavaScript na Web - armazenando dados no navegador/js/main.js
@@ -0,0 +1,98 @@
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+
+// Se houver itens já salvos no Local Storage, a lista é iniciada com esses valores.
+// Caso não hajam itens, a lista é inicializada vazia.
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach((item) => {
+    console.log(item);
+    criaElemento(item);
+})
+
+form.addEventListener("submit", (evento) => {
+    // Previne o comportamento padrão do evento.
+    evento.preventDefault(); 
+
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    // Percorre o array de itens e procura pelo nome igual
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+    console.log(existe)
+    
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if(existe){
+        itemAtual.id = existe.id;
+
+        atualizaElemento(itemAtual);
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+    } else {
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
+
+        criaElemento(itemAtual);
+
+        itens.push(itemAtual);
+    }
+
+    // Utilizamos o JSON Stringify para converter o nosso objeto numa string, porque o LocalStorage só armazena string.
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+
+    // Limpa os campos do formulário após criar um novo elemento
+    nome.value = "";
+    quantidade.value = "";
+
+})
+
+function criaElemento(item) {
+    // console.log(nome);
+    // console.log(quantidade);
+
+    // <li class="item"><strong>7</strong>Camisetas</li>
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
+    // Coloco o elemento strong dentro do elemento li
+    novoItem.appendChild(numeroItem);
+    
+    novoItem.innerHTML += item.nome;
+
+    novoItem.appendChild(botaoDeleta(item.id));
+
+    lista.appendChild(novoItem);
+    // console.log(novoItem);
+
+    
+}
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement('button');
+    elementoBotao.innerText = "X";
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    })
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id){
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+}
